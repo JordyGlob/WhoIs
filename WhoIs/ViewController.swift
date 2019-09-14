27818420b4ffb15396082ba9765305e9800232da
Jordy Xavier Pazaran Reyes <diablitoco@live.com.mx>
2019-09-14 13:10:06 -0500

    Initial Commit

---
+++ WhoIs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhoIs
+//
+//  Created by braky on 9/14/19.
+//  Copyright © 2019 braky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
